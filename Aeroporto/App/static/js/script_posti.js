3d353d0f15b2c1a76f89c2dfda45dd0589df2afb
LoreGerm <lorenzogermano146@gmail.com>
2022-03-31 17:02:53 +0200

    .

---
+++ Aeroporto/App/static/js/script_posti.js
@@ -0,0 +1,26 @@
+let n = 10;
+for (let i = 1; i <= n; i++) {
+    document.getElementById('posti').append(fila(i));
+}
+function fila(i) {
+    const node = document.createElement('tr');
+    let td = '<th scope="row">' + i + '</th>'
+    let lettere = ['A', 'B', 'C', 'D', 'E', 'F'];
+    for (let j = 0; j < 6; j++) {
+        td += '<td><button id="' + i.toString() + lettere[j] + '" class="btn" onclick="scelta(this.id)"><img src="{% static "img/poltrona.png" %}" height=30 width=30></button></td>';
+    }
+    node.innerHTML = td;
+    return node;
+}
+
+let posti = [];
+function scelta(id) {
+    if (document.getElementById(id).classList == 'btn') {
+        posti.push(id);
+        document.getElementById(id).classList.add('btn-success');
+    }
+    else {
+        posti.pop(id);
+        document.getElementById(id).classList.remove('btn-success');
+    }
+}
