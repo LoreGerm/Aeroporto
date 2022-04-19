b54278e2d6bf9181b858c63b2e39e8745f27fceb
LoreGerm <lorenzogermano146@gmail.com>
2022-04-19 11:49:16 +0200

    .

--- Aeroporto/App/views.py
+++ Aeroporto/App/views.py
@@ -195,9 +195,11 @@ def i_tuoi_voli(request):
 
 def cancella_prenotazione(request,id):
     pren = Prenotazioni.objects.get(id = id)
+    utente = Utente.objects.get(id = pren.utente.id)
     volo = Volo.objects.get(id = pren.volo.id)
     posti_pren = len(list(pren.posti_prenotati.split(',')))
     volo.posti_totali += posti_pren
+    utente.delete()
     pren.delete()
     volo.save()
     
@@ -318,12 +320,14 @@ def elimina_aeroporto(request, id):
 
 def elimina_prenotazione(request, id):
     pren = Prenotazioni.objects.get(id = id)
+    utente = Utente.objects.get(id = pren.utente.id)
     volo = Volo.objects.get(id = pren.volo.id)
     posti_pren = len(list(pren.posti_prenotati.split(',')))
     volo.posti_totali += posti_pren
+    utente.delete()
     pren.delete()
     volo.save()
-    
+
     return redirect('gestione_prenotazioni')
 
 
